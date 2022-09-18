505b876d8882b114465ed4d91791507acff93e7e
Benjamin Cheng <ben@bcheng.me>
2022-09-18 15:28:44 -0400

    lab1: init from course

---
+++ lab1/targets/target2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int
+foo ( char *arg )
+{
+	char	buf[260];
+	int	p, j, min, len;
+
+	p = 280;
+	min = (strlen(arg) > p) ? p : strlen(arg);
+	len = min;
+  
+	for (j = 0; j <= len; j++)
+		buf[j] = arg[j];
+
+	return (0);
+}
+
+int
+lab_main ( int argc, char *argv[] )
+{
+	int	t = 2;
+
+	printf ("Target2 running.\n");
+
+	if (argc != t)
+	{
+		fprintf ( stderr, "target2: argc != 2\n" );
+		exit ( EXIT_FAILURE );
+	}
+
+	foo ( argv[1] );
+
+	return (0);
+}
