0fb51f9febbe229292b41585372102df0015ab62
Benjamin Cheng <ben@bcheng.me>
2022-10-11 13:32:08 -0400

    lab2/part2: implement driver program

--- lab2/part2/driver.c
+++ lab2/part2/driver.c
@@ -1,10 +1,42 @@
-#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include "ip_parser.h"
 
-int main(int argc, char * argv[])
-{
-	FILE *fileptr = fopen(argv[1], "rb");         
-	// write your code here
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <input_file>\n", argv[0]);
+		return -1;
+	}
+
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		return -1;
+	}
+
+	struct stat stat;
+	int res = fstat(fd, &stat);
+	if (res == -1) {
+		perror("fstat");
+		return -1;
+	}
+
+	uint8_t *buffer = malloc(stat.st_size);
+	if (buffer == NULL) {
+		fprintf(stderr, "malloc failed\n");
+		return -1;
+	}
+
+	res = read(fd, buffer, stat.st_size);
+	if (res == -1) {
+		perror("read");
+		return -1;
+	}
+
+	res = ip_parse(buffer, stat.st_size);
+	printf("ip_parse returned %d\n", res);
+
 	return 0;
 }
-
